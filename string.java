75b5c82604aaa1ede0632fd62e4fccdc39aecf1c
Amith.P <158695565+vulture02@users.noreply.github.com>
2025-04-13 09:38:54 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,14 @@
+package DSA;
+
+public class string {
+    public static void main(String[] args) {
+        String a="Amith";
+        System.out.println(a);
+        a="ananya";//created new object not changed the string a value
+        System.out.println(a);
+        String name=new String("amith");
+
+
+
+    }
+}
