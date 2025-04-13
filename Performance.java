c6062d83e25fe2919a3e9c78f8d24f7a30bf0e3e
Amith.P <158695565+vulture02@users.noreply.github.com>
2025-04-13 09:39:50 +0530

    Add files via upload

---
+++ Performance.java
@@ -0,0 +1,12 @@
+package DSA;
+public class Performance {
+    public static void main(String[] args) {
+        String series="";
+        for (int i=0;i<26;i++){
+            char ch=(char) ('a'+i);
+            System.out.println(ch);
+            series+=ch;
+        }
+        System.out.println(series);
+    }
+}
---
+++ PrettyPrinting.java
@@ -0,0 +1,10 @@
+package DSA;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a=4503.25621f;
+        System.out.printf("formated string is %.2f\n",a);
+        System.out.printf("pie %.2f\n",Math.PI);
+        System.out.printf("hello my name is %s i am %s","amith","cse");
+    }
+}
