b10e274f12898954971107391f8253038ed271b9
Amith.P <158695565+vulture02@users.noreply.github.com>
2025-04-16 20:53:09 +0530

    Add files via upload

---
+++ partten1star.java
@@ -0,0 +1,20 @@
+package DSA;
+
+public class partten1star {
+//    public static void main(String[] args) {
+//        for (int i=0;i<=4;i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+public static void main(String[] args) {
+    for (int i=1;i<=5;i++) {
+        for (int j = 1; j < i; j++) {
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+}
+}
